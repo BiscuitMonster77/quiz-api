3b4346ed9444af56dcc48884011550f51025bd19
CameronStamp <68219671+CameronStamp@users.noreply.github.com>
2021-05-20 11:52:51 +0100

    initial yaml config

--- src/tests/quiz.nextStageAsync.spec.js
+++ src/tests/quiz.nextStageAsync.spec.js
@@ -78,6 +78,12 @@ describe('quiz.nextStageAsync()', function () {
             await expect(quiz.nextStageAsync('1234')).to.eventually.be.rejectedWith('Not enough players yet');
             expect(saveInstance.notCalled).to.be.true;
         });
+
+        it('shouldrejectrequestifonlyoneplayer', async function () {
+            instanceBefore.players=[{name:"Player1"}];//Oneplayers
+            await expect(quiz.nextStageAsync('1234')).to.eventually.be.rejectedWith('Not enough players yet');
+            expect(saveInstance.notCalled).to.be.true;
+        });
     });
 
     describe('when showing question', function() {
